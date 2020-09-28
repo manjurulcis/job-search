8d06e723d4a3494c99f70c3b7312804d24226d04
Md Manzurul Haque <haque.mdmanzurul@gmail.com>
2020-09-29 01:57:28 +0300

    Fix migration and seeder

---
+++ database/seeders/JobSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+class JobSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i < 100; $i++) {
+            DB::table('jobs')->insert([
+                'title' => Str::random(10),
+                'description' => Str::random(100),
+                'company' => Str::random(100),
+                'reference' => Hash::make(Str::random(10)),
+            ]);
+        }
+        
+    }
+}
