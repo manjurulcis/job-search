d782b98f8382e6424435ae3f50816f8a4bfdf882
Md Manzurul Haque <haque.mdmanzurul@gmail.com>
2020-10-01 01:14:27 +0300

    Automated testing setup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\DB;
 use App\Models\Job;
 use Carbon\Carbon;
 
